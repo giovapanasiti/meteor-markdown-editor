7aa183f440f706218549b4829f0929fac7c32eac
Giovanni <me@giovanni.com>
2017-01-17 22:32:58 +0100

    first commit

---
+++ client/components/accounts.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Template} from 'meteor/templating';
+import {Blaze} from 'meteor/blaze';
+
+class Accounts extends Component {
+    componentDidMount(){
+        // This thing is gonna be called only one time as soon as the component is rendered
+
+        /*render the blaze accounts form then find the div we just rendered in the render method
+        and place the Blaze accounts form in that div*/
+
+
+    }
+
+    componentWillUnmount() {
+        // This thing is going to be called as soon as the component is going to be removed from the screen
+
+        /*Go find the forms we created and destroy them. We need to clean up those forms ourselves*/
+
+
+    }
+
+    render() {
+        return (
+            /*Place for some JSX*/
+            <div></div>
+        );
+    }
+};
+
+export default Accounts;
