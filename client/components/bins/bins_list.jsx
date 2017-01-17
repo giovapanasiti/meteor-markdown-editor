38595aa06037c6534967e42cc6d09e99a46bc17e
Giovanni <me@giovanni.com>
2017-01-17 23:43:11 +0100

    adding react router

--- client/components/bins/bins_list.jsx
+++ client/components/bins/bins_list.jsx
@@ -3,10 +3,30 @@ import {createContainer} from 'meteor/react-meteor-data';
 import {Bins} from '../../../imports/collections/bins';
 
 class BinsLists extends Component {
+    onBinRemove(bin) {
+        Meteor.call('bins.remove', bin);
+    }
+
+    renderList() {
+        return this.props.bins.map(bin=>{
+            return (
+                <li className="list-group-item" key={bin._id}>
+                    <strong>Bin:</strong> {bin._id}
+                    <span className="pull-right">
+                        <button className="btn btn-danger" onClick={()=>{this.onBinRemove(bin)}}>
+                            Remove
+                        </button>
+                    </span>
+                </li>
+            )
+        })
+    }
     render() {
-        console.log(this.props.bins)
+        console.log(this.props.bins);
         return(
-            <div></div>
+            <ul className="list-group">
+                {this.renderList()}
+            </ul>
         )
     }
 }
---
+++ client/components/bins/bins_main.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class BinsMain extends Component {
+    render(){
+        return(
+            <div>This is BinsMain</div>
+        )
+    }
+}
+
+export default BinsMain;
